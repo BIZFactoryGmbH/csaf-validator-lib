4eee261cf190f463f4701d87260b4cd5471a8a5e
Rene Schultheis <schultheisrene@gmail.com>
2024-09-25 11:01:52 +0200

    fix tests

--- tests/shared/validVersionTests.js
+++ tests/shared/validVersionTests.js
@@ -1,30 +1,22 @@
-import { expect } from "chai"
+import { expect } from 'chai'
 
 /**
  * @param {string} version
  */
-export default function areVersionTestsValid(version) {
-    return describe(`Duplicate tests for version ${version}`, function () {
-        it(`no duplicate tests in /versions/${version}/basic.js`, async function () {
-            const tests = Object.values(
-                await import(
-              `../../versions/basic.js`
-            ))
+export default async function areVersionTestsValid(version) {
+  const tests = Object.values(await import(`../../versions/basic.js`))
 
-            let versionTests = []
-            try {
-                versionTests = Object.values(
-                    await import(
-                    `../../versions/${version}/basic.js`
-                ))
-            } catch (e) {
-                throw new Error(`No basic tests found for version ${version}`)
-            }
+  let versionTests = []
+  try {
+    versionTests = Object.values(
+      await import(`../../versions/${version}/basic.js`)
+    )
+  } catch (e) {
+    throw new Error(`No basic tests found for version ${version}`)
+  }
 
-            // Filter out all duplicate tests
-            const uniqueTests = versionTests?.filter(test => tests.includes(test))
+  // Filter out all duplicate tests
+  const uniqueTests = versionTests?.filter((test) => tests.includes(test))
 
-            expect(uniqueTests?.length).to.be.eq(0)
-        })
-    })  
+  return uniqueTests?.length === 0
 }
--- tests/uniqueVersionTests.js
+++ tests/uniqueVersionTests.js
@@ -1,7 +1,14 @@
-import areVersionTestsValid from "./shared/validVersionTests.js";
+import { expect } from 'chai'
+import areVersionTestsValid from './shared/validVersionTests.js'
 
-export default [
-    areVersionTestsValid("1.2"),
-    areVersionTestsValid("2.0"),
-    areVersionTestsValid("2.1"),
-]
+describe('Duplicate tests for versions', () => {
+  it('no duplicate tests in /versions/1.2/basic.js', async () => {
+    expect(await areVersionTestsValid('1.2')).to.be.true
+  })
+  it('no duplicate tests in /versions/2.0/basic.js', async () => {
+    expect(await areVersionTestsValid('2.0')).to.be.true
+  })
+  it('no duplicate tests in /versions/2.1/basic.js', async () => {
+    expect(await areVersionTestsValid('2.1')).to.be.true
+  })
+})
