bdeff774051f33d5343cb1f3206dfc2a49fe2e77
Rene Schultheis <schultheisrene@gmail.com>
2024-09-25 14:02:07 +0200

    export all test types

--- getVersionTests.js
+++ getVersionTests.js
@@ -1,10 +1,15 @@
 import * as basic from './versions/basic.js'
+import * as informative from './versions/informativeTests.js'
+import * as mandatory from './versions/mandatoryTests.js'
+import * as schema from './versions/schemaTests.js'
+import * as optional from './versions/optionalTests.js'
 
 /**
  * @param {string} version
+ * @param {unknown[]} tests
  */
-export default async function getVersionTests(version) {
-  let TESTS = Object.values(basic)
+export default async function getVersionTests(version, tests) {
+  let TESTS = tests
   let VERSION_TESTS = []
   let IGNORED_TESTS = []
 
@@ -20,16 +25,16 @@ export default async function getVersionTests(version) {
       )
     )
   } catch (error) {
-    throw new Error(
-      `Could not load tests for version ${version}. Falling back to basic tests.`
+    console.error(
+      `Could not load version specific tests for version ${version}.`
     )
   }
 
-  TESTS = TESTS.filter((t) => !IGNORED_TESTS.includes(t.name))
+  TESTS = TESTS.filter((/** @type {any} */t) => !IGNORED_TESTS.includes(t.name))
 
   // Make sure that the tests are not duplicated
   VERSION_TESTS.forEach((t) => {
-    if (!TESTS.map((t) => t.name).includes(t.name)) {
+    if (!TESTS.map((/** @type {any} */t) => t.name).includes(t.name)) {
       TESTS.push(t)
     } else {
       // eslint-disable-next-line no-console
@@ -39,3 +44,9 @@ export default async function getVersionTests(version) {
 
   return TESTS
 }
+
+export const getVersionBasicTests = (/** @type {string} */ version) => getVersionTests(version, Object.values(basic))
+export const getVersionOptionalTests = (/** @type {string} */ version) => getVersionTests(version, Object.values(optional))
+export const getVersionMandatoryTests = (/** @type {string} */ version) => getVersionTests(version, Object.values(mandatory))
+export const getVersionSchemaTests = (/** @type {string} */ version) => getVersionTests(version, Object.values(schema))
+export const getVersionInformativeTests = (/** @type {string} */ version) => getVersionTests(version, Object.values(informative))
