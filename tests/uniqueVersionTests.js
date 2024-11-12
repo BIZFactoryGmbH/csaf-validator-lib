18565a32eadea1d57b5375203b0673dc7a2a3b41
Rene Schultheis <schultheisrene@gmail.com>
2024-11-12 13:42:59 +0100

    fix code test coverage

--- tests/uniqueVersionTests.js
+++ tests/uniqueVersionTests.js
@@ -1,7 +1,17 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
 import areVersionTestsValid from './shared/validVersionTests.js'
+import ChaiAsPromised from 'chai-as-promised'
+
+chai.use(ChaiAsPromised)
 
 describe('Duplicate tests for versions', () => {
+  it('no tests for unavailable version', async () => {
+    areVersionTestsValid('1.2').then(
+      (result) => {
+          result.should.throw(Error, 'No basic tests found for version 1.2');
+      }
+    )
+  })
   it('no duplicate tests in /versions/2.0/basic.js', async () => {
     expect(await areVersionTestsValid('2.0')).to.be.true
   })
