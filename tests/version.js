c7a14b0d20cfb58df2706887a46c6e7aba18f88c
Rene Schultheis <schultheisrene@gmail.com>
2024-11-25 11:28:42 +0100

    load tests accordingly

--- tests/version.js
+++ tests/version.js
@@ -1,11 +1,12 @@
 import { expect } from 'chai'
 import validate from '../validate.js'
 import minimalVexDoc from './shared/minimalVexDoc.js'
-import { getVersionBasicTests, getVersionMandatoryTests } from '../getVersionTests.js'
+import { getVersionFullTests, getVersionMandatoryTests } from '../getVersionTests.js'
 
 describe('Version specific tests', function () {
   it('invalid document with basic test', async function () {
-    const tests = Object.values(await import('../versions/basic.js'))
+    /** @type {any[]} */
+    const tests = await getVersionFullTests('2.0')
 
     const result = await validate(tests, {
       ...minimalVexDoc,
@@ -20,7 +21,7 @@ describe('Version specific tests', function () {
 
   it('invalid document, but one test is ignored', async function () {
     /** @type {any[]} */
-    const tests = await getVersionMandatoryTests('2.1')
+    const tests = await getVersionFullTests('2.1')
 
     // We expect the document to be valid, because the test is ignored
     const result = await validate(tests, {
